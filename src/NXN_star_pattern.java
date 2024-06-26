01e8cb141a074e4bc67d22de4567d1b67f08d205
nova <jegannova2174@gmail.com>
2024-06-26 19:28:27 +0530

    by using java

--- src/NXN_star_pattern.java
+++ src/NXN_star_pattern.java
@@ -1,3 +1,37 @@
 public class NXN_star_pattern {
+        public static void getStarPattern(int n) {
+            for(int i=0;i<n;i++)
+            {
+                if(i==0 || i==n-1)
+                {
+                    for(int j=0;j<n;j++)
+                    {
+                        System.out.print("* ");
+                    }
+                    System.out.println();
+                }
+                else
+                {
+                    for(int j=0;j<=0;j++)
+                    {
+                        System.out.print("* ");
+                    }
+                    for(int j=0;j<n;j++)
+                    {
+                        System.out.print(" ");
+                    }
+                    for(int j=0;j<=0;j++)
+                    {
+                        System.out.print(" *");
+                    }
+                    System.out.println();
 
-}
+                }
+            }
+        }
+
+    public static void main(String[] args) {
+        getStarPattern(5);
+    }
+    }
+
