418c0488287ebff0aa20f9bea9f2c317d5cb8fc2
nova <jegannova2174@gmail.com>
2024-07-11 19:31:03 +0530

    by using java

--- src/selection_sort.java
+++ src/selection_sort.java
@@ -10,11 +10,12 @@ public class selection_sort {
             for(j=i+1;j<a.length;j++){
                 if(a[j]<a[small])
                 {
-                    int temp=a[i];
-                    a[i]=a[j];
-                    a[j]=temp;
+                    small=j;
                 }
             }
+            int temp=a[small];
+            a[small]=a[i];
+            a[i]=temp;
         }
         return a;
     }
@@ -22,11 +23,11 @@ public class selection_sort {
 
 
     public static void main(String[] args) {
-        int a[]={3,6,10,2,1,14,8};
+        int a[]={77,80,10,2,1,14,8};
         int res[]=sorted_array(a);
         for(int i=0;i<a.length;i++)
         {
-            System.out.println(res[i]);
+            System.out.print(res[i]+" ");
         }
     }
 }
