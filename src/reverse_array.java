5b59e3db952779321a1fd98cc42e0d378e9ece53
nova <jegannova2174@gmail.com>
2024-08-11 16:57:28 +0530

    by using java

--- src/reverse_array.java
+++ src/reverse_array.java
@@ -5,20 +5,20 @@ public class reverse_array {
     static void reverse(int[] a,int n)
     {
         int j=n-1;
-        for(int i=0;i<n;i++)
-        {
-           if(i<j)
+        int i=0;
+           while(i<j)
            {
                int temp=a[i];
                a[i]=a[j];
                a[j]=temp;
                j--;
+               i++;
 
            }
-        }
-        for(int i=0;i<n;i++)
+
+        for(int k=0;k<n;k++)
         {
-            System.out.print(a[i]+" ");
+            System.out.print(a[k]+" ");
         }
     }
 
@@ -28,4 +28,5 @@ public class reverse_array {
         int[] a={3,2,9,8,7,1};
         reverse(a,a.length);
     }
+
 }
